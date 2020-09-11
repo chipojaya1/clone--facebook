47451e27c5ab3e7c08b25e4627eef50bfa1a674f
chipojaya1 <jaya@diveintocode.jp>
2020-09-11 05:29:13 +0000

    css and formatting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,11 +50,11 @@ class PostsController < ApplicationController
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def post_params
-      params.require(:post).permit(:posts, :image, :image_cache)
-    end
+  def post_params
+    params.require(:post).permit(:posts, :image, :image_cache)
+  end
 end
